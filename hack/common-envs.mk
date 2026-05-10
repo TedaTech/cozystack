0ad0168a95cb29d072a6cc8fdff7904a76f1ba68
Freek Gruntjes <freek@gruntjes.net>
2026-05-10 22:20:08 +0200

    chore: configure TedaTech release registry

--- hack/common-envs.mk
+++ hack/common-envs.mk
@@ -5,7 +5,7 @@ else
     SED_INPLACE := sed -i
 endif
 
-REGISTRY ?= ghcr.io/cozystack/cozystack
+REGISTRY ?= ghcr.io/tedatech/cozystack
 TAG = $(shell git describe --tags --exact-match 2>/dev/null || echo latest)
 PUSH := 1
 LOAD := 0
@@ -15,7 +15,7 @@ BUILDX_EXTRA_ARGS ?=
 COZYSTACK_VERSION = $(patsubst v%,%,$(shell git describe --tags))
 
 BUILDX_ARGS := --provenance=false --push=$(PUSH) --load=$(LOAD) \
-  --label org.opencontainers.image.source=https://github.com/cozystack/cozystack \
+  --label org.opencontainers.image.source=https://github.com/TedaTech/cozystack \
   $(if $(strip $(BUILDER)),--builder=$(BUILDER)) \
   $(if $(strip $(PLATFORM)),--platform=$(PLATFORM)) \
   $(BUILDX_EXTRA_ARGS)
@@ -30,4 +30,3 @@ ifeq ($(COZYSTACK_VERSION),)
     $(shell git fetch upstream --tags)
     COZYSTACK_VERSION = $(patsubst v%,%,$(shell git describe --tags))
 endif
-
